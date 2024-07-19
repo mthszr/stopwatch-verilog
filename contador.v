4f20272bfb57d0cbfe329dec891a9d9a2f1eb097
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-07-19 08:55:07 -0300

    updating the counting logic

--- contador.v
+++ contador.v
@@ -1,46 +1,60 @@
 module contador (
     input clk,
     input reset,
-    output reg [9:0] cont_seg, //contador para os segundos
-    output reg [3:0] cont_dec	//contador para os decimos de segundo
+    input conta,
+    input pausa,
+    input para,
+
+    output reg [9:0] cont_seg, // contador para os segundos
+    output reg [3:0] cont_dec // contador para os decimos de segundo
 );
 
-//
-initial begin 
-	cont_seg = 0;
-	cont_dec = 0;
-end
+wire [2:0] estado_atual;
+wire contando;
 
-reg [31:0] cont_clk = 0; //contador para os ciclos do clock
+estados(
+    .clk(clk),
+    .reset(reset),
+    .conta(conta),
+    .pausa(pausa),
+    .para(para),
+    .estado(estado_atual),
+    .contando(contando)
+);
 
+reg [31:0] cont_clk = 0; // contador para os ciclos do clock
 
-parameter segundo = 5000000; //ciclos de clock por segundo
+parameter segundo = 10; // ciclos de clock por segundo
 parameter decimo = segundo / 10;
 
-always @(posedge clk) begin
+initial begin 
+    cont_seg = 0;
+    cont_dec = 0;
+end
 
-	cont_clk <= cont_clk + 1;
-	
-	if (!reset) begin
-		cont_clk <= 32'd0;
-		cont_seg <= 10'd0;
-		cont_dec <= 4'd0;
-	end
-	
-	if (cont_clk == decimo) begin
-		cont_clk <= 1;
-		if (cont_dec < 4'd9) begin //verifique se passaram 10 decimos de segundo
-			cont_dec <= cont_dec + 1; //se for falso, acrescente 1 ao contador dos decimos de segundo
-		end else begin 
-			cont_dec <= 4'd0; //se for verdade, coloque zero no contador dos decimos de segundo
-			if (cont_seg <= 10'd999) begin //verifque se passaram 999 segundos
-				cont_seg <= cont_seg + 1; //se for falso, acrescente 1 ao contador dos segundos
-			end else begin
-				cont_seg <= 10'd0;//se for verdade, reinicie a contagem
-			end
-		end
-	end
-	
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
+        cont_clk <= 32'd0;
+        cont_seg <= 10'd0;
+        cont_dec <= 4'd0;
+    end else begin
+        if (contando) begin
+            cont_clk <= cont_clk + 1;
+            if (cont_clk == decimo) begin
+                cont_clk <= 1;
+                if (cont_dec < 4'd9) begin // verifique se passaram 10 decimos de segundo
+                    cont_dec <= cont_dec + 1; // se for falso, acrescente 1 ao contador dos decimos de segundo
+                end else begin 
+                    cont_dec <= 4'd0; // se for verdade, coloque zero no contador dos decimos de segundo
+                    if (cont_seg < 10'd999) begin // verifique se passaram 999 segundos
+                        cont_seg <= cont_seg + 1; // se for falso, acrescente 1 ao contador dos segundos
+                    end else begin
+                        cont_seg <= 10'd0; // se for verdade, reinicie a contagem
+                    end
+                end
+            end
+        end
+    end
 end
 
 endmodule
--- estados.v
+++ estados.v
@@ -1,69 +1,72 @@
 module estados(
-	input clk,
-	input reset,
-	input conta,
-	input pausa,
-	input para,
-	
-	output reg [2:0] estado
+    input clk,
+    input reset,
+    input conta,
+    input pausa,
+    input para,
+    
+    output reg [2:0] estado,
+    output reg contando
 );
 
 parameter inicio = 0, contar = 1, pausar = 2, parar = 3;
-reg [2:0] estado_atual = inicio; // inicia o cronometro parado com 0
+reg [2:0] estado_atual = inicio; // inicia o cronometro parado
 
-always @(posedge clk or negedge reset) begin // parte sequencial
-	if (!reset) begin
-		estado_atual <= inicio;
-	end
-	else begin
-		case (estado_atual)
-			inicio: begin
-				if (conta == 0) begin
-					estado_atual <= contar;
-				end
-			end
-			contar: begin
-				if (reset == 0) begin
-					estado_atual <= inicio;
-				end 
-				else if (pausa == 0) begin
-					estado_atual <= pausar;
-				end 
-				else if (para == 0) begin
-					estado_atual <= parar;
-				end
-			end
-			pausar: begin
-				if (reset == 0) begin
-					estado_atual <= inicio;
-				end
-				else if (conta == 0) begin
-					estado_atual <= contar;
-				end
-				else if (para == 0) begin
-					estado_atual <= parar;
-				end
-			end
-			parar: begin
-				if (reset == 0) begin
-					estado_atual <= inicio;
-				end
-				else if (conta == 0) begin
-					estado_atual <= contar;
-				end
-			end
-		endcase
-	end
+always @(posedge clk or negedge reset) begin // troca de estados
+    if (!reset) begin
+        estado_atual <= inicio;
+        contando <= 0;
+    end else begin
+        case (estado_atual)
+            inicio: begin
+                if (conta == 0) begin
+                    estado_atual <= contar;
+                    contando <= 1;
+                end
+            end
+            contar: begin
+                if (reset == 0) begin
+                    estado_atual <= inicio;
+                    contando <= 0;
+                end else if (pausa == 0) begin
+                    estado_atual <= pausar;
+                    contando <= 1;
+                end else if (para == 0) begin
+                    estado_atual <= parar;
+                    contando <= 0;
+                end
+            end
+            pausar: begin
+                if (reset == 0) begin
+                    estado_atual <= inicio;
+                    contando <= 0;
+                end else if (conta == 0) begin
+                    estado_atual <= contar;
+                    contando <= 1;
+                end else if (para == 0) begin
+                    estado_atual <= parar;
+                    contando <= 0;
+                end
+            end
+            parar: begin
+                if (reset == 0) begin
+                    estado_atual <= inicio;
+                    contando <= 0;
+                end else if (conta == 0) begin
+                    estado_atual <= contar;
+                    contando <= 1;
+                end
+            end
+        endcase
+    end
 end
 
-
 always @(posedge clk or negedge reset) begin
-	if (!reset) begin
-		estado <= inicio;
-	end
-	else begin
-		estado <= estado_atual;
-	end
+    if (!reset) begin
+        estado <= inicio;
+    end else begin
+        estado <= estado_atual;
+    end
 end
 
 endmodule
